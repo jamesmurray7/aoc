a2a4b9e2769b4e86604ef972e6f55163b8feb29b
parqueted <j.murray7@ncl.ac.uk>
2021-12-09 14:54:02 +0000

    day9 part b

--- 2021/day9.R
+++ 2021/day9.R
@@ -40,4 +40,6 @@ library(raster)
 library(igraph)
 adj <- raster(mat.store!=9)
 basins <- as.matrix(clump(adj, directions = 4)) # a matrix of basins
-
+basins.coordinates <- lapply(1:max(basins, na.rm = T), function(x) which(basins == x, arr.ind = T))
+basins.size <- lapply(basins.coordinates, nrow)
+prod(sort(do.call(c, basins.size), decreasing = T)[1:3])
